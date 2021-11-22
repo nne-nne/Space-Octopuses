37ec0fe7f0fc38f2d99adc4cb370b619a804a8ae
nne-nne <adam.leczkowski1@gmail.com>
2021-11-22 22:25:33 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>
+    /// zmienna 'speed' będzie decydować o szybkości gracza.
+    /// float, czyli liczba zmiennoprzecinkowa.
+    /// public, czyli będzie widoczna w Unity w oknie Inspektora.
+    /// 
+    /// AMBITNIEJ:
+    /// [SerializeField] private float speed;
+    /// tak zadeklarowana zmienna też jest widoczna w Inspektorze, a dodatkowo jest prywatna.
+    /// </summary>
+    public float speed;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Space Octopuses
+ The project is a part of Unity course made by Vertex Science Club at Gdansk University of Technology
