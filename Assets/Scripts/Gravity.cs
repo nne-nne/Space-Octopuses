6a23b45f0a6ee82b1caba730e6223971b9a58874
nne-nne <adam.leczkowski1@gmail.com>
2021-11-23 15:27:15 +0100

    comments

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-    public float g = 9.81f;
+    // nasza wymyślona stała grawitacyjna
+    public float g;
 
     private Rigidbody rb;
+
+    // w tym miejscu deklarujemy listę planet. To nie wystarczy, żeby móc jej użyć!
     private List<GameObject> planetsInRange;
     
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        // po zadeklarowaniu listy, trzeba ją najpierw stworzyć:
         planetsInRange = new List<GameObject>();
+
+        // komponentu rigidbody oczywiście nie tworzymy, ale pobieramy referencję do tego, który już istnieje
+        rb = GetComponent<Rigidbody>();
     }
 
     
@@ -20,13 +26,21 @@ public class Gravity : MonoBehaviour
     {
         foreach(GameObject planet in planetsInRange)
         {
-            // kierunek działania siły to po prostu znormalizowana różnica wektorów położenia
+            // kierunek działania siły to po prostu różnica wektorów położenia. Normalizujemy go dla wygody.
             Vector3 direction = (planet.transform.position - transform.position).normalized;
+
+            // ta linijka mówi sama za siebie
             float distance = Vector3.Distance(transform.position, planet.transform.position);
+
+            // zgodnie ze wzorem F=GmM/r^2 aplikujemy siłę grawitacji
             rb.AddForce(direction * g * rb.mass * planet.GetComponentInParent<Rigidbody>().mass / (distance * distance), ForceMode.Force);
         }
     }
 
+
+    /// <summary>
+    /// jeśli wpadniemy na trigger i okaże się on planetą, dodajmy tę planetę do naszej listy
+    /// </summary>
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Gravity"))
@@ -35,6 +49,9 @@ public class Gravity : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// analogicznie jeśli wyjdziemy z pola grawitacyjnego planety, wyrzućmy ją z listy
+    /// </summary>
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Gravity"))
